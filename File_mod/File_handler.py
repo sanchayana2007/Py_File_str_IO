049f0c4b102943758f1eedf325fa56ddf3cfea75
sanchayana2007 <sanchayana2007@gmail.com>
2014-01-09 21:05:30 +0530

    file changes skeletal done

--- File_mod/File_handler.py
+++ File_mod/File_handler.py
@@ -8,6 +8,10 @@ class FileHandler(object):
 		self.FileHandlerError = "No error"
 		self.Intial_file_name = ""
 		self.fd_read = ""
+		self.file_name_1 = ""
+		self.file_name_2 = ""
+		
+		
 	'''Method for checking the file Authenticity'''
 	def file_open(self,filename1,filename2,filename3):
 		if filename1 == "":
@@ -15,12 +19,23 @@ class FileHandler(object):
 		else:
 			self.Intial_file_name = filename1
 			self.fd_read = open(self.Intial_file_name,'r')
-			if fd_read == "":
-				"got to throw an exceptrion while Opening"
+			if self.fd_read == "":
+				"got to throw an exception while Opening"
 				print "Not able to open the file "
+			else:
+				self.file_name_1 = open(filename2,'w+')
+				self.file_name_2 = open(filename3,'w+')
+				return self.fd_read
+			
+						
 			
 	'''Method for Reading the file lien will return an List of the words'''
 	def read_line(self):
 			line= fd_read.readline()
 			return line
+		
+	def close_all_files(self):
+		self.fd_read.close()
+		self.file_name_1.close()
+		self.file_name_2.close()
 		
--- main.py
+++ main.py
@@ -5,7 +5,13 @@ this will have the parser Logic for the functions '''
 from File_mod import File_handler
 import parser
 
-def __init__(self):
+if __name__ == '__main__':
+	List_of_lines = []
 	'''Read the file and get a List for a Line '''
-	
- 
+	fo = File_handler.FileHandler()
+	fd_read = fo.file_open("test.txt","test2.txt","test3.txt")
+	for line in fd_read:
+		print line
+		List_of_lines.append(line)
+	fo.close_all_files()
+		
