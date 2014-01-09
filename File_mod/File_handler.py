0e924004e8543dd41a0cc807366476c4f0fde82d
sanchayana2007 <sanchayana2007@gmail.com>
2014-01-09 18:06:52 +0530

    Code -1
    Added skeletal changes for the File Module and the Main module .

--- File_mod/File_handler.py
+++ File_mod/File_handler.py
@@ -1,4 +1,26 @@
 '''Author : Sanche 
 This module will handle the file I/O Operations and cretaion of new files 
 '''
-import fileinput 
+
+class FileHandler(object):
+	'''Method for checking the file Authenticity'''
+	def __init__(self):
+		self.FileHandlerError = "No error"
+		self.Intial_file_name = ""
+		self.fd_read = ""
+	'''Method for checking the file Authenticity'''
+	def file_open(self,filename1,filename2,filename3):
+		if filename1 == "":
+			print "file for reading is not provided"
+		else:
+			self.Intial_file_name = filename1
+			self.fd_read = open(self.Intial_file_name,'r')
+			if fd_read == "":
+				"got to throw an exceptrion while Opening"
+				print "Not able to open the file "
+			
+	'''Method for Reading the file lien will return an List of the words'''
+	def read_line(self):
+			line= fd_read.readline()
+			return line
+		
---
+++ main.py
@@ -0,0 +1,11 @@
+
+''' Author: Sanche
+this will have the parser Logic for the functions '''
+
+from File_mod import File_handler
+import parser
+
+def __init__(self):
+	'''Read the file and get a List for a Line '''
+	
+ 
