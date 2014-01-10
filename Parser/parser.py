dc64530b9aa517362c7fb919919c1ed0633c3963
sanchayana2007 <sanchayana2007@gmail.com>
2014-01-10 19:52:24 +0530

    parsing changes

--- Parser/parser.py
+++ Parser/parser.py
@@ -3,3 +3,30 @@
 this will have the parser Logic for the functions '''
 
 import re
+
+class Parser(object):
+	def __init__(self):
+		self.elements_Vowels = []
+		self.elements_Inverted = []
+		self.arrayofVowels = ['a','e','i','o','u']
+		
+	def Check_Vowels(self,line):
+		'''pattern_space = re.split("(?:(?:[^a-zA-Z]+')|(?:'[^a-zA-Z]+))|(?:[^a-zA-Z']+)", line)'''
+		pattern_space = re.compile("([\w][\w]*'?\w?)").findall(line)
+		if pattern_space:
+			'''print 'found',pattern_space'''
+			for word in pattern_space:
+				
+				print word[0]
+				for Vchck in self.arrayofVowels:
+					if word[0] == Vchck:
+							print 'Vowel'
+							self.elements_Vowels.append(word)
+					else:
+						self.elements_Inverted = word[0]
+							
+		else:
+			print 'Not found'
+		
+	
+		
--- main.py
+++ main.py
@@ -3,23 +3,28 @@
 this will have the parser Logic for the functions '''
 
 from File_mod import File_handler
-import parser
+import Parser.parser
 from optparse import OptionParser
 import fileinput 
 
 def main():
-''' Usage , pattern and version are predefined atributes for OPtionParser'''
-	parser = OptionParser(usage="%prog -e PATTREN",version="%prog 1.0")
+	''' Usage , pattern and version are predefined atributes for OPtionParser'''
+	opparser = OptionParser(usage="%prog -e PATTREN",version="%prog 1.0")
 	''' Store is REGEXPR and -e , will store the value in regexpr If not given then default is false and help '''
-	parser.add_option("-e","--regexpr",dest="regexpr",action="store", default=False ,help="pattern expresion for match ")
-	opt,args = parser.parse_args()
+	opparser.add_option("-e","--regexpr",dest="regexpr",action="store", default=False ,help="pattern expresion for match ")
+	opt,args = opparser.parse_args()
+	
 	List_of_lines = []
 	'''Read the file and get a List for a Line '''
 	fo = File_handler.FileHandler()
 	fd_read = fo.file_open("test.txt","test2.txt","test3.txt")
 	for line in fd_read:
-		print line
+		'''print line'''
 		List_of_lines.append(line)
+		Lineparser = Parser.parser.Parser()
+		Lineparser.Check_Vowels(line)
+		
+		
 	fo.close_all_files()
 		
 
